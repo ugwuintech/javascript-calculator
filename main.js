6da5f09fa73280d3c9efc4392d27305daaa1924f
Joseph Ugwu <josephchukwuebukaugwu@gmail.com>
2022-12-16 13:02:07 -0800

    adding functionaties

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
+// DISPLAY;
 const display = document.querySelector("#display");
+const history = document.querySelector("#display-1");
 const clicked = (num) => {
   console.log(num);
   display.value += num;
 };
+
+// EQUAL TO
+function equalTo() {
+  const displayValue = display.value;
+  const result = eval(displayValue);
+  display.value = result;
+  history.value = displayValue;
+}
+
+// TOGGLE
 const toggleTheme = () => {
   const theme = localStorage.getItem("theme");
   if (theme == null) {
